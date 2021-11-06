8cd011dad0b41003a53171761c8ae054389b8603
Muhammet <muhammetacin@icloud.com>
2021-11-06 10:08:46 +0100

    created index page for book list and added the functionality to get list of books from the database

---
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public IndexModel(ApplicationDbContext dbContext)
+        {
+            applicationDbContext = dbContext;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Books = await applicationDbContext.Book.ToListAsync();
+        }
+    }
+}
