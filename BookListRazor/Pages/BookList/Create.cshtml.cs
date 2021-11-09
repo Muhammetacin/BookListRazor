4838b9892e77d22e4c3aede9a60375c49d0e47d8
Muhammet <muhammetacin@icloud.com>
2021-11-09 17:08:00 +0100

    Added ISBN property to Book class and updated database accordingly with migration. Created the Create page for book

---
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public CreateModel(ApplicationDbContext applicationDb)
+        {
+            _applicationDbContext = applicationDb;
+        }
+
+        public Book Book { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
