90850d9ab7bd0f741b91ed282d8c21a3177483de
Muhammet <muhammetacin@icloud.com>
2021-11-09 23:24:10 +0100

    Controller added for book and new method GetAll returns books in a list

---
+++ BookListRazor/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        
+        public BookController(ApplicationDbContext applicationDb)
+        {
+            _applicationDbContext = applicationDb;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _applicationDbContext.Book.ToList() });
+        }
+    }
+}
